7ba81630bdf41f45c89cd90fd78cf16cb45d788e
tschweitzer <t.schweitzer@arcor.de>
2012-01-31 16:29:05 +0100

    Display a pop up message on page load, which fades away.

--- index.php
+++ index.php
@@ -17,6 +17,20 @@ include("defaultHeadInclude.php");
     </style>
 
 	<script type="text/javascript">
+		// From https://gist.github.com/716577
+		function jqmSimpleMessage(message) {
+		    $("<div class='ui-loader ui-overlay-shadow ui-body-b ui-corner-all'><h1>" + message + "</h1></div>")
+		        .css({
+		            display: "block",
+		            opacity: 0.85,
+		            top: window.pageYOffset+100
+		        })
+		        .appendTo("body").delay(3000)
+		        .fadeOut(400, function(){
+		            $(this).remove();
+		        });
+		}
+
 		$(document).ready(function() {
 		    $('#loginDialogLoginButton').live('click',function() {
 			    var username = $('#loginDialogUsernameInput').val();
@@ -49,6 +63,8 @@ include("defaultHeadInclude.php");
 					});
 			    }
 		    });
+		    
+		    jqmSimpleMessage('This is just a test!');
 	  }); 
 	</script>
 </head>
